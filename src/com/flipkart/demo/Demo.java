6cd21df3c45402da2cf405fe849288c8b4dc95ac
unknown <atmakursreemeghana22@gmail.com>
2022-01-19 15:40:40 +0530

    My first commit

---
+++ src/com/flipkart/demo/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author DELL
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first demo");
+	}
+
+}
